d1499b4ee043d9bf3f34f8a129723fdd21ff4e61
Sergio Regueiro Martinez <sergioregueiro12@gmail.com>
2026-09-17 00:06:54 +0200

    Skeleton

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import App from './App.tsx'
 import './i18n'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <main className="min-h-screen bg-neutral-100 text-neutral-900 transition-colors duration-200 px-4 py-12 dark:bg-neutral-950 dark:text-neutral-50 sm:px-6 lg:px-8">
+                <App />
+        </main>
+    </StrictMode>,
 )
